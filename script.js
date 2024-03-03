03095fb1103158ac3b68d129351504843c1818eb
DinhAnhWibuboiz <lam03022001@gmail.com>
2024-03-03 17:10:50 +0700

    update birth date

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Ngày bắt đầu (tháng được đánh số từ 0 đến 11)
   var startDate = new Date(2023, 9, 28); // Tháng 9 là tháng 10
-  
+
   // Ngày sinh của mỗi người
-  var birhdate_1 = new Date(2001, 2, 2);
-  var birhdate_2 = new Date(2002, 3, 12);
-  
+  var birthday_L = new Date(2001, 3, 3);
+  var birthday_Y = new Date(2002, 4, 12);
+
   // Hàm cập nhật số ngày
   function updateCountdown() {
     var currentDate = new Date();
@@ -13,22 +13,49 @@ document.addEventListener("DOMContentLoaded", function() {
     var daysDiff = Math.floor(timeDiff / (1000 * 3600 * 24)); // Sử dụng Math.floor thay vì Math.ceil để lấy nguyên
     document.getElementById('countdown').textContent = daysDiff + ' days';
   }
-  
+
   // Hàm tính tuổi
   function calculateAge(birthdate) {
     var today = new Date();
     var age = today.getFullYear() - birthdate.getFullYear();
-    var m = today.getMonth() - birthdate.getMonth();
+    // Tháng nó bắt đầu từ số 0 ạ (0,1,2,3,4,5,6,7,8,9,10,11) nên phải + 1 mới đúng
+    var m = (today.getMonth() + 1) - birthdate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthdate.getDate())) {
-        age--;
+      age--;
     }
     return age;
   }
-  
-  // Cập nhật số ngày mỗi giây
+
+  function checkBirthday(birthday, id, name) {
+    var today = new Date();
+    // Tháng nó bắt đầu từ số 0 ạ (0,1,2,3,4,5,6,7,8,9,10,11) nên phải + 1 mới đúng
+    if ((today.getMonth() + 1) == birthday.getMonth() && today.getDate() == birthday.getDate()) {
+      document.getElementById(id).className = "birthday-wish"
+      document.getElementById(id).innerHTML = "Happy birthday "+ name +" 🎂🎂! " ;
+    } else {
+      document.getElementById(id).className = "birthday-wish-end"
+      document.getElementById(id).innerHTML = "" ;
+    }
+  }
+
+  function setBirthDate(birthday_L, birthday_Y) {
+    document.getElementById('Y').textContent = calculateAge(birthday_Y);
+    document.getElementById('L').textContent = calculateAge(birthday_L);
+  }
+
+  // Cập nhật mỗi giây
   setInterval(updateCountdown, 1000);
-  
-  // Cập nhật tuổi ban đầu
-  document.getElementById('L').textContent = calculateAge(birhdate_1);
-  document.getElementById('Y').textContent = calculateAge(birhdate_2);
+
+  setInterval(function () {
+    checkBirthday(birthday_Y, "birth_date", "Yên");
+  }, 1000);
+
+  setInterval(function () {
+    checkBirthday(birthday_L, "birth_date", "Lam");
+  }, 1000);
+
+  setInterval(function () {
+    setBirthDate(birthday_L, birthday_Y);
+  }, 1000);
+
 });
